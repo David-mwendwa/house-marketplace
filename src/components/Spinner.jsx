7122f0fefcb2caeebb7ec936201f66881b466586
techdave <klatzdavie19@gmail.com>
2022-02-27 08:36:41 +0300

    add privateRoute component and create AuthStatus hook

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import spinner from '../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <div className='loadingSpinnerContainer'>
+      <div className="loadingSpinner">
+        {/* <img src={spinner} alt="loading..." style={{width:'100px', margin:'auto', display:'block'}} /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
